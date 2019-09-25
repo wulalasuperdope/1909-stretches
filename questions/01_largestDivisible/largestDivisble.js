40112f96486a0727618a5118dc9026c38ef2493c
Russell Moore <russellwmoore@gmail.com>
2019-09-25 15:43:20 -0400

    add examples to largestDivisible

--- questions/01_largestDivisible/largestDivisble.js
+++ questions/01_largestDivisible/largestDivisble.js
@@ -1,8 +1,12 @@
 /*
 largestDivisible returns the largest number from a list of numbers where that number is divisible by a passed in number
 write the largestDivisible function
+
 */
 
 function largestDivisible(arr, num) {}
+
+// console.log(largestDivisible([1, 2, 3, 4, 5, 6, 7], 3));//6
+// console.log(largestDivisible([1], 2));//null
 
 module.exports = { largestDivisible };
