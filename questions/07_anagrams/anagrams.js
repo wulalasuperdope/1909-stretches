78fe0cbeb6651412bf48d8b201020741942eba5d
Cissy Bai <cissy5120@gmail.com>
2019-10-14 19:07:31 -0400

    updated anagrams solution

--- questions/07_anagrams/anagrams.js
+++ questions/07_anagrams/anagrams.js
@@ -6,16 +6,20 @@ const checkAnagrams = (str1, str2) => {
   if(str1.length !== str2.length){
     return false;
   }else{
-    let str1Arr = str1.split('');
-    let str2Arr = str2.split('');
-    str1Arr.forEach(element => {
-      for(let i = str2Arr.length - 1; i > -1; i--){
-        if(element !== str2Arr[i]){
-          return false;
-        }
-      }
-    });
-    return true;
+    // let str1Arr = str1.split('');
+    // let str2Arr = str2.split('');
+    // str1Arr.forEach(element => {
+    //   for(let i = str2Arr.length - 1; i > -1; i--){
+    //     if(element !== str2Arr[i]){
+    //       return false;
+    //     }
+    //   }
+    // });
+    // return true;
+    str1 = str1.split('').sort().join('');
+    str2 = str2.split('').sort().join('');
+    return str1 === str2;
+    
   }
 
 };
