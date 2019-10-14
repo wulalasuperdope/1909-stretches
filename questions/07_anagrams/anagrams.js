a564359590220e4c16d1cd012a00dd32c3cf2bd7
HypeDis <30463842+HypeDis@users.noreply.github.com>
2019-10-14 18:45:32 -0400

    deleted old solns (#4)

---
+++ questions/07_anagrams/anagrams.js
@@ -0,0 +1,8 @@
+// Given two strings check if they are anagrams of each other
+// Return a boolean
+
+const checkAnagrams = (str1, str2) => {
+  //YOUR CODE
+};
+
+module.exports = { checkAnagrams };
