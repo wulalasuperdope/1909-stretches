f89fc3385ad0bcf8c2a775df289b5a285eacb9b0
hypedis <m.bae@live.com>
2019-10-15 14:16:09 -0400

    added dice roller to stretches

---
+++ questions/10_diceRoller/DiceRoller.js
@@ -0,0 +1,9 @@
+// Create a class called DiceRoller
+// It should contain a roll method and a
+// history property which outputs all of the previous rolls
+
+class DiceRoller {
+  //YOUR CODE
+}
+
+module.exports = { DiceRoller };
