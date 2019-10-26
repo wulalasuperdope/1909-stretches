290988c2047f9bdbc14ff10d87c2e079b467749b
hypedis <m.bae@live.com>
2019-10-26 10:13:15 -0400

    caesers cypher

---
+++ questions/11_caeser_cypher/caeser_cypher.js
@@ -0,0 +1,23 @@
+// Caeser Cipher
+// https://en.wikipedia.org/wiki/Caesar_cipher
+// create a function that takes a string and a shift value and returns an encrypted string
+
+// accept only alphanumeric characters
+// shifted values should wrap back around
+// z -> a  and 9 -> 0
+
+// example
+// str = 'hello'
+// shift = 3
+// result = 'khoor'
+
+// example
+// str = "yolo789"
+// shift = 4
+// result = "csps123"
+
+const encryptString = (str, shift) => {
+  // YOUR CODE
+};
+
+module.exports = { encryptString };
